99ea2bca773831b2b59e53a10125527525d2fc44
Nirvan <nir1r7@users.noreply.github.com>
2024-01-28 23:55:38 -0500

    Fixed page padding. Added basic colour scheme tests. Added babel depedency, unsure as to how the program will react.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,11 +3,12 @@ import {Navbar} from './navigation';
 function App() {
   return (
     <div className="App">
+      {/* <Navbar className/> */}
       <div className="main">
-        <Navbar />
+        <Navbar className/>
 
         <br/>
-        <p className='fella'>
+        <p className='fella red-text'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
@@ -17,16 +18,16 @@ function App() {
           tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
           quia. 
         </p>
-        <p className='fella'>Commodi minima excepturi repudiandae velit hic maxime
+        <p className='fella red-bg'>Commodi minima excepturi repudiandae velit hic maxime
           doloremque. Quaerat provident commodi consectetur veniam similique ad 
           earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
           fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
           suscipit quas? Nulla, placeat.</p>
-        <p className='fella'>Voluptatem quaerat non architecto ab laudantium
+        <p className='fella night-text'>Voluptatem quaerat non architecto ab laudantium
           modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
           totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
           quasi aliquam eligendi, placeat qui corporis!</p>
-        <p className='fella'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        <p className='fella night-bg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
           optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
@@ -34,22 +35,29 @@ function App() {
           nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
           tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
           quia. </p>
-        <p className='fella'>Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+        <p className='fella vista-text'>Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
           sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
           recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
           minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
           quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
           fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
           consequuntur! </p>
-        <p className='fella'>Commodi minima excepturi repudiandae velit hic maxime
+        <p className='fella vista-bg'>Commodi minima excepturi repudiandae velit hic maxime
           doloremque. Quaerat provident commodi consectetur veniam similique ad 
           earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
           fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
           suscipit quas? Nulla, placeat.</p>
-        <p className='fella'>Voluptatem quaerat non architecto ab laudantium
+        <p className='fella cyan-text'>Voluptatem quaerat non architecto ab laudantium
           modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
           totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
           quasi aliquam eligendi, placeat qui corporis!</p>
+        <p className='fella cyan-bg'>molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia.</p>
       </div>
     </div>
   );
--- src/components/app/navigation/index.js
+++ src/components/app/navigation/index.js
@@ -6,15 +6,15 @@ export function Navbar(){
         <div className='sticky-top'>
             <nav className='navbar navbar-expand-lg blur'>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#1">Nir1</a>
+                    <a className="navbar-brand link-danger" href="#1">Nir1</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
-                            <NavLink href="#2" title="Home"/>
-                            <NavLink href="#3" title="Projects"/>
-                            <NavLink href="#4" title="Socials"/>
+                            <NavLink href="#1" title="Home"/>
+                            <NavLink href="#2" title="Projects"/>
+                            <NavLink href="#3" title="Socials"/>
                         </ul>
                     </div>
                 </div>
@@ -27,7 +27,7 @@ function NavLink(props){
     return(
         <li className="nav-item">
             <span className="nav-link">
-                <a className="link-offset-2 link-underline-dark link-underline-opacity-0 link-underline-opacity-100-hover link-dark" href={props.href}>
+                <a className="link-offset-2 link-underline-danger link-underline-opacity-0 link-underline-opacity-100-hover red-text" href={props.href}>
                     {props.title}
                 </a>
             </span>
